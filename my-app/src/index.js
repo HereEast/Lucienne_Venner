be59482b4ef3a21baf8b9e3cd6b7fca5420a1f8a
Here East <nastiapiven@gmail.com>
2021-08-18 16:15:37 +0200

    feat: set SASS

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,1 @@
-import image from './images/lazy.png';
-
-const subHeader = document.createElement('h2');
-subHeader.innerHTML = 'This elements was created by js';
-
-const myImage = new Image();
-myImage.src = image;
-
-document.body.appendChild(subHeader);
-document.body.appendChild(myImage);
+import './sass/index.scss';
